27e69bedc066754b928c921df082aed56683e304
christijohnson <christiajohnson@gmail.com>
2022-05-20 21:17:16 -0600

    enhancement to pages

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -16,3 +16,20 @@ const weekday = dayofweek[currentdate.getDay()];
 document.querySelector('#currentDate').textContent = `${weekday}, ${day} ${monthname} ${year}`;
 document.querySelector('#updated').textContent = document.lastModified;
 document.querySelector('#currentyear').textContent = currentdate.getFullYear();
+
+//const feedbackElement = document.getElementById('feedback');
+  // get the form so we can read what was entered in it.
+  //const formElement = document.forms[0];
+  // add a 'listener' to wait for a submission of our form. When that happens run the code below.
+  if (weekday == 'Monday' || 'Tuesday' || 'Friday') {
+    document.getElementById("inviteBanner").innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+  //formElement.addEventListener('submit', function(e) {
+    //stop the form from doing the default action
+    //e.preventDefault();
+    // set the contents of our feedback element to a message letting the user know the form was submitted successfully. Notice that we pull the name that was entered in the form to personalize the message!
+    //feedbackElement.innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    // make the feedback element visible.
+    //feedbackElement.style.display = "block";
+    // add a class to move everything down so our message doesn't cover it.
+    //document.body.classList.toggle('moveDown');
+   }
