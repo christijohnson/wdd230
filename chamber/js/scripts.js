5346b59d133581e6b6f818a5a0d449197587f2db
christijohnson <christiajohnson@gmail.com>
2022-05-28 17:37:49 -0600

    chamber home page updates

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -1,3 +1,4 @@
+/* - hamburger button and navigation - */
 function toggleMenu () {
     document.getElementById('primNav').classList.toggle('open');
     document.getElementById('hamBtn').classList.toggle('open');
@@ -6,6 +7,7 @@ function toggleMenu () {
 const x = document.getElementById('hamBtn');
 x.onclick = toggleMenu; 
 
+/* - Date banner and last modified date - */
 const currentdate = new Date();
 const year = currentdate.getFullYear();
 const day = currentdate.getDate();
@@ -17,19 +19,10 @@ document.querySelector('#currentDate').textContent = `${weekday}, ${day} ${month
 document.querySelector('#updated').textContent = document.lastModified;
 document.querySelector('#currentyear').textContent = currentdate.getFullYear();
 
-//const feedbackElement = document.getElementById('feedback');
-  // get the form so we can read what was entered in it.
-  //const formElement = document.forms[0];
-  // add a 'listener' to wait for a submission of our form. When that happens run the code below.
-  if (weekday == 'Monday' || weekday == 'Tuesday') {
-    document.getElementById("inviteBanner").innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
-  //formElement.addEventListener('submit', function(e) {
-    //stop the form from doing the default action
-    //e.preventDefault();
-    // set the contents of our feedback element to a message letting the user know the form was submitted successfully. Notice that we pull the name that was entered in the form to personalize the message!
-    //feedbackElement.innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
-    // make the feedback element visible.
-    //feedbackElement.style.display = "block";
-    // add a class to move everything down so our message doesn't cover it.
-    //document.body.classList.toggle('moveDown');
-   }
+/* - conditional banner - */
+if (currentdate.getDay() == 1 || currentdate.getDay() == 5) {
+    document.getElementById('inviteBanner').innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    document.getElementById('inviteBanner').classList.remove('hide');
+   } else {document.getElementById('inviteBanner').classList.value('hide');}
+
+/* - windchill - */
